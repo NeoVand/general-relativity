7d6a1fa563a59f83f37ffa6ec1fb6513a446f52f
NeoVand <mmv@mit.edu>
2026-09-09 16:10:07 -0500

    Add a GitHub repository link to the reading navbar

--- src/SafeHTML.svelte
+++ src/SafeHTML.svelte
@@ -3,7 +3,7 @@
  let {html}=$props();
  function render(node){
   // Book content, model replies, and TeX are always sanitized before insertion.
-  node.replaceChildren(DOMPurify.sanitize(html,{RETURN_DOM_FRAGMENT:true,ADD_TAGS:['annotation','semantics'],ADD_ATTR:['encoding','tabindex'],USE_PROFILES:{html:true,svg:true,svgFilters:true,mathMl:true}}));
+  node.replaceChildren(DOMPurify.sanitize(html,{RETURN_DOM_FRAGMENT:true,ADD_TAGS:['annotation','semantics'],ADD_ATTR:['encoding','tabindex','target'],USE_PROFILES:{html:true,svg:true,svgFilters:true,mathMl:true}}));
   return ()=>node.replaceChildren();
  }
 </script>
